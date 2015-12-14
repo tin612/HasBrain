6ae4f594d255df6863cc7cdf658786386637d13c
Phan Thanh Tín <phantin612@gmail.com>
2015-12-14 17:46:53 +0700

    upload source code

---
+++ app/src/main/java/com/tin612/hasbrain/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tin612.hasbrain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
